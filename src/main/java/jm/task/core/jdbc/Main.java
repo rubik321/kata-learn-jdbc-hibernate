c4234db41c504098597c39f20789b29e1572d19d
Danil Pudovkin <dvpudovkin@gmail.com>
2022-07-26 20:45:11 +0500

    feat: add UserServiceImpl class in order to abstract from DAO class
    Add some simple checks in Main class. All tests are passed.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        String testName = "Ivan";
+        String testLastName = "Ivanov";
+        byte testAge = 5;
+
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser(testName, testLastName, testAge);
+        userService.saveUser(testName, testLastName, testAge);
+        userService.saveUser(testName, testLastName, testAge);
+        userService.getAllUsers().forEach(Main::printUser);
+        userService.removeUserById(3);
+        userService.getAllUsers().forEach(Main::printUser);
+        userService.cleanUsersTable();
+        System.out.println(userService.getAllUsers().size());
+
+    }
+
+    private static void printUser(User u) {
+        System.out.println(u.getName() + ", " + u.getLastName() + ", " + u.getAge());
     }
 }

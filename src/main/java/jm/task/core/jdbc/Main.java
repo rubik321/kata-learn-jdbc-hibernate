3312550e5b7ad05433a45026314a94aaf6318cd7
Danil Pudovkin <dvpudovkin@gmail.com>
2022-07-26 21:04:32 +0500

    refactor: change logic in Main class according to task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,30 +4,28 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
 
-        String testName = "Ivan";
-        String testLastName = "Ivanov";
-        byte testAge = 5;
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ruslan", "Abramov", (byte) 23));
+        users.add(new User("Roman", "Perov", (byte) 21));
+        users.add(new User("Oksana", "Zhdanova", (byte) 39));
+        users.add(new User("Dmitriy", "Komarov", (byte) 35));
 
-        userService.dropUsersTable();
         userService.createUsersTable();
-        userService.saveUser(testName, testLastName, testAge);
-        userService.saveUser(testName, testLastName, testAge);
-        userService.saveUser(testName, testLastName, testAge);
-        userService.getAllUsers().forEach(Main::printUser);
-        userService.removeUserById(3);
-        userService.getAllUsers().forEach(Main::printUser);
+        for (User user: users) {
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
+            System.out.println("User с именем – " + user.getName() + " добавлен в базу данных");
+        }
+        for (User user: userService.getAllUsers()) {
+            System.out.println(user);
+        }
         userService.cleanUsersTable();
-        System.out.println(userService.getAllUsers().size());
-
-    }
-
-    private static void printUser(User u) {
-        System.out.println(u.getName() + ", " + u.getLastName() + ", " + u.getAge());
+        userService.dropUsersTable();
     }
 }

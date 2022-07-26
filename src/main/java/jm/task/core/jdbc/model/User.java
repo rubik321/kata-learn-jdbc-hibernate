19d5f9a331b899b1f3303fdd7889f65dbcc22792
Danil Pudovkin <dvpudovkin@gmail.com>
2022-07-26 21:06:39 +0500

    refactor: override User::toString to print user in readable format

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -59,4 +59,9 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User{%s, %s, %d}".formatted(getName(), getLastName(), getAge());
+    }
 }

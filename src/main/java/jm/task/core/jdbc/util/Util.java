3410adbde722aaf94a28b907336546145669d442
Danil Pudovkin <dvpudovkin@gmail.com>
2022-07-26 15:36:44 +0500

    feat: add method getConnection() to class Util
    This method implements creating connection to MySQL DB.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public final static String URL = "jdbc:mysql://localhost::3306/preproject_db";
+    public final static String USERNAME = "admin";
+    public final static String PASSWORD = "admin1234@";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            Driver driver = new com.mysql.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            if (!connection.isClosed()) {
+                System.out.println("Connection is established!");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Can't load driver class!");
+        }
+
+        return connection;
+    }
 }

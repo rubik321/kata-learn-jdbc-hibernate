d5d3b119217ce7c5ddc674264b02cd32bb87dda8
Danil Pudovkin <dvpudovkin@gmail.com>
2022-07-28 00:20:24 +0500

    feat: add Util::getSessionFactory method
    Implements getting SessionFactory in hibernate.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     public final static String URL = "jdbc:mysql://localhost:3306/preproject_db";
@@ -27,5 +36,35 @@ public class Util {
         }
 
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        SessionFactory sessionFactory = null;
+
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, URL);
+        settings.put(Environment.USER, USERNAME);
+        settings.put(Environment.PASS, PASSWORD);
+        settings.put(Environment.SHOW_SQL, true);
+        settings.put(Environment.FORMAT_SQL, true);
+        settings.put(Environment.DEFAULT_SCHEMA, "preproject_db");
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+        Configuration configuration = new Configuration();
+        configuration.setProperties(settings);
+        configuration.addAnnotatedClass(User.class);
+
+        final ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        try {
+           sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException e) {
+            StandardServiceRegistryBuilder.destroy(serviceRegistry);
+        }
+
+        return sessionFactory;
     }
 }
